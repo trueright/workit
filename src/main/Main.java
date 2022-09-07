c4873e5aacf5add36fe7233c1186084cb84b6a33
trueright <109972957+trueright@users.noreply.github.com>
2022-09-07 23:28:43 +0900

    Add files via upload

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ public class Main {
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("OMG IT WORKS!");
+        window.setTitle("OMG IT WORKS! Fixed!");
         
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
